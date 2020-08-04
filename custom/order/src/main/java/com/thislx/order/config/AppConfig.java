332bfe552dcc81ef22391d1763625c38492681ee
LiXiang <ixiangx@leimingtech.com>
2020-08-04 10:26:29 +0800

    图片路径修改

--- custom/order/src/main/java/com/thislx/order/config/AppConfig.java
+++ custom/order/src/main/java/com/thislx/order/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.thislx.user;
+package com.thislx.order.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * 配置类
+ *
  * @author lixiang
  * @version V1.0
  * @date 2020/4/19 18:20
@@ -13,13 +15,26 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    /**
+     * 配置RestTemplate
+     *
+     * @date 2020/4/29 15:53
+     * @author lixiangx@leimingtech.com
+     **/
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+
+    /**
+     * 配置tomcat端口
+     *
+     * @date 2020/4/29 15:53
+     * @author lixiangx@leimingtech.com
+     **/
     @Bean
-    public TomcatServletWebServerFactory tomcat(){
+    public TomcatServletWebServerFactory tomcat() {
         TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
         tomcatServletWebServerFactory.setPort(8080);
         return tomcatServletWebServerFactory;
